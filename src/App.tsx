1542534fe56711e2c8ce897f2b18776f3e568ce2
Pasquale D'Silva <accounts@psql.me>
2024-04-03 22:16:01 -0400

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,82 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import tanaki from './assets/tanaki-avatar.png'
+import whatbro from './assets/what-bro.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
 
-      <div className="width-xl">
-        <div className="pt-20 text-center body">
-          <img
-            className="inline-block text-center w-80"
-            src={tanaki}
-            alt="tanaki"
-          />
-          <div>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="max-w-xl pt-10">
+          <img className="block mx-auto w-80" src={tanaki} alt="tanaki" />
+          <div className="text-center">
+            {' '}
             <div className="pb-2 text-2xl">
               Brand mascots run communities with
             </div>
             <div className="text-5xl font-bold">HostOS®</div>
+            <div className="inline-block px-4 py-2 mt-2 bg-yellow-300 rounded-full">
+              beta
+            </div>
           </div>
 
-          <div className="mt-20 text-3xl">
-            <p>
-              Mascots brought to life with HostOS® are the ultimate brand
-              ambassadors.
+          <div className="mt-24 text-center text-gray-500">
+            <p className="text-4xl text-black">
+              The ultimate brand ambassador.
             </p>
-            <p>
-              Trained to work out of the box. <br /> Ready to learn your brand
-              personality.
-            </p>
+            <p className="mt-2 text-2xl">Trained to work out of the box.</p>
+            <p className="text-2xl">Ready to learn your brand personality.</p>
           </div>
-          <div>
-            We’ve spent hundreds of hours training our base mascots to be
-            collaborative and fun
+          <div className="text-4xl text-center pt-36">
+            We’ve spent thousands of hours training our base mascots to be{' '}
+            <span className="text-orange-500 ">collaborative</span> &{' '}
+            <span className="text-purple-500 ">fun</span>.
           </div>
-          <div>but to take it further...</div>
-          <div>we offer a</div>
-          <div>mascot training ground</div>
+          <div className="mt-8 text-2xl text-center text-gray-500">
+            but to take it further...
+          </div>
+          <div className="text-2xl text-center text-gray-500">we offer a</div>
+          <br />
+          <br />
+          <div className="italic font-bold text-center text-9xl">
+            MASCOT TRAINING GROUND
+          </div>
+          <br />
+          <div className="flex justify-center mt-48">
+            <img
+              className="block w-100"
+              src={whatbro}
+              alt="what does that mean bro?"
+            />
+          </div>
+          <div className="mt-48 text-center">
+            <div className="text-4xl font-bold">Train by Speaking</div>
+            <div className="mt-2 text-2xl text-gray-500">
+              Teach your Host to care for your customers in a language they
+              understand. Personality, memes, in-jokes, the things that make
+              your brand shine.
+              <br />
+              <br />
+              It's as easy as speaking... because that's how you do it!
+            </div>
+          </div>
+
+          <div className="mt-48 text-center">
+            <div className="text-4xl font-bold">Cross-Platform</div>
+            <div className="mt-2 text-2xl text-gray-500">
+              Hosts bring customers together, meeting them where they already
+              are.
+              <br />
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className="pt-40 text-center">
+        {' '}
+        everything has a butt. this is the butt of the website.
       </div>
     </>
   )
