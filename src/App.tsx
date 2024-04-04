4275d48b07b9731c7b994f7f3e8f99b38b2f6990
Pasquale D'Silva <accounts@psql.me>
2024-04-03 20:28:06 -0400

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header';
+import Header from './components/Header'
+import tanaki from './assets/tanaki-avatar.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header/>
+      <Header />
 
-      <div className="body">
-      <span>hello friends</span>
+      <div className="width-xl">
+        <div className="pt-20 text-center body">
+          <img
+            className="inline-block text-center w-80"
+            src={tanaki}
+            alt="tanaki"
+          />
+          <div>
+            <div className="pb-2 text-2xl">
+              Brand mascots run communities with
+            </div>
+            <div className="text-5xl font-bold">HostOS®</div>
+          </div>
+
+          <div className="mt-20 text-3xl">
+            <p>
+              Mascots brought to life with HostOS® are the ultimate brand
+              ambassadors.
+            </p>
+            <p>
+              Trained to work out of the box. <br /> Ready to learn your brand
+              personality.
+            </p>
+          </div>
+          <div>
+            We’ve spent hundreds of hours training our base mascots to be
+            collaborative and fun
+          </div>
+          <div>but to take it further...</div>
+          <div>we offer a</div>
+          <div>mascot training ground</div>
+        </div>
       </div>
     </>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 import logo from '../assets/logo.svg'
+import tanaki from '../assets/tanaki-avatar.png'
 
 export default function Header() {
   return (
-    <div className="w-full">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center">
-          <img src={logo} alt="Lingonberry Logo" className="mr-4 h-14" />
-          <div>
-            <h1 className="text-3xl font-bold">Lingonberry Intelligence</h1>
-            <span className="text-xl">リンゴンベリーインテリジェンス</span>
-          </div>
-        </div>
-        <nav className="flex space-x-4 text-lg font-bold">
+    <div className="flex flex-col items-center w-full text-center">
+      {' '}
+      <img src={logo} alt="Lingonberry Logo" className="h-16 mb-4" />{' '}
+      <div>
+        <h1 className="text-3xl font-bold">Lingonberry Intelligence</h1>
+        <span className="text-xl text-gray-400">
+          リンゴンベリーインテリジェンス
+        </span>
+        <nav className="flex mt-2 space-x-4 text-lg font-bold">
           <a href="https://tanaki.ai/">Tanaki</a>
           <a href="#">HostOS</a>
           <a href="#">Careers</a>
