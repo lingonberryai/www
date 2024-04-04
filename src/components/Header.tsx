c59e00b538011587293c83685f4cfef6122ae746
Pasquale D'Silva <accounts@psql.me>
2024-04-03 20:03:21 -0400

    prettier

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,22 @@
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.svg'
 
 export default function Header() {
   return (
-    <div className='bg-red-300'>
-      <div className='float-left bg-red-300'>
-        <img src={logo} alt="Lingonberry Logo" className="float-left mr-4 h-14" />
-        <div>
-          <h1 className='text-3xl font-bold'>Lingonberry Intelligence</h1>
-          <span className='text-xl'>リンゴンベリーインテリジェンス</span>
+    <div className="w-full">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <img src={logo} alt="Lingonberry Logo" className="mr-4 h-14" />
+          <div>
+            <h1 className="text-3xl font-bold">Lingonberry Intelligence</h1>
+            <span className="text-xl">リンゴンベリーインテリジェンス</span>
+          </div>
         </div>
-      </div>
-      <div className='float-right bg-blue-400'>
-      <span>hi links here</span>
+        <nav className="flex space-x-4 text-lg font-bold">
+          <a href="https://tanaki.ai/">Tanaki</a>
+          <a href="#">HostOS</a>
+          <a href="#">Careers</a>
+          <a href="#">Investors</a>
+        </nav>
       </div>
     </div>
   )
